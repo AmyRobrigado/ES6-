e5d2178c1519e46d7faacce8c2b11955669bab1e
AmyRobrigado <amyantanod@gmail.com>
2022-12-16 12:46:58 -0500

    Completed ex 1

--- Exercises/exercise1.js
+++ Exercises/exercise1.js
@@ -2,10 +2,22 @@
 
 class Employee {
     constructor(id, firstName, lastName, jobTitle, payRate) {
-    this.employeeId = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.jobTitle = jobTitle;
-    this.payRate = payRate;
+        this.employeeId = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobTitle = jobTitle;
+        this.payRate = payRate;
     }
-    }
+}
+/* 
+let employee1 = new Employee(
+    1, "Ian", "Auston", "Graphic Artist", 42.50);
+console.log(`Employee ${employee1.firstName} created`);
+console.log(`Job title is ${employee1.jobTitle}`);
+console.log(`Pay rate is $${employee1.payRate}`); */
+
+let employee2 = new Employee(
+    2, "Tristan", "Mackie", "Software Developer", 30.57);
+console.log(`Employee ${employee2.firstName} created`);
+console.log(`Job title is ${employee2.jobTitle}`);
+console.log(`Pay rate is $${employee2.payRate}`); 
