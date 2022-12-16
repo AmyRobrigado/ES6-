6a72858fe8361514f0d77125c252305c4694b2e7
AmyRobrigado <amyantanod@gmail.com>
2022-12-16 12:32:59 -0500

    Initial Commit

---
+++ Exercises/exercise1.js
@@ -0,0 +1,11 @@
+"use strict"
+
+class Employee {
+    constructor(id, firstName, lastName, jobTitle, payRate) {
+    this.employeeId = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.jobTitle = jobTitle;
+    this.payRate = payRate;
+    }
+    }
